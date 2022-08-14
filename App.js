8e43387bbe8fbd3cb92859b46e69afca7fed33f3
Pedro Torques <pedro_torques@estudante.sesisenai.org.br>
2022-08-14 14:07:45 -0300

    experimentação

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <LinearGradient
+        colors={['#0f96bf', '#043b99']}
+        start={{
+          x: 0,
+          y: 1
+        }}
+        end={{
+          x: 0,
+          y: 0
+        }}
+        style={styles.background}
+      >
+
+        <View style={styles.body}>
+          <View style={styles.border}>
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </View>
+      </LinearGradient>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  body: {
+    width: "100%",
+    height: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  border: {
+    backgroundColor: "transparent",
+    width: "90%",
+    height: "80%",
+    marginTop: 100,
+    borderWidth: 2,
+    borderColor: "#EC3DC5",
+    textAlign: "center"
+  },
+
+  title: {
+    color: "white",
+    fontSize: 30,
+    margin: "auto",
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+# Aplicativo Acústica
+## Iniciação científica
+### Escola S - Florianópolis, SC, Brasil
+
+Aplicativo para a utilização de modelos preditivos de ruído de tráfego baseados em variáveis coletadas em situações reais.
